f014b3f072dca70ad2c230b515e81bef03a81161
syumai6842 <syuumai6842@gmail.com>
2024-08-23 19:55:13 +0900

    新しく作成したテンプレート用に書かれていたコードを移植しました。 主な変更点は ・index.tsxが無くなった ・App.tsx -> page.tsx ・App.css -> globals.css ・mybotan.tsxに"use client"を追加しました。詳しくは以下の解説

---
+++ src/app/component/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+export class Footer extends React.Component{
+
+
+    render(): React.ReactNode {
+        return(
+            <div className="App">
+                Footer 
+            </div>
+        );
+    }
+}
