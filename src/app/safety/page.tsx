fb429a2356b36e915cebd45e0bbe0b1e49b2a9dc
2304井上明 <kmc2304@kamiyama.ac.jp>
2024-09-11 21:36:32 +0900

    evacuation page under construction

--- src/app/safety/page.tsx
+++ src/app/safety/page.tsx
@@ -1,20 +1,24 @@
 // src/app/safety/page.tsx
+'use client';
 import React from 'react';
 import "./safetypage.css";
+import { useRouter } from 'next/navigation';
 
-const SafetyPage = () => {
+export default function safety() {
+  const router = useRouter();
+
   return (
     <div className="App">
       <div className="safetybutton-container">
-        <button className="safetybutton-button" id="darkred">
-        救助が必要
+        <button className="safetybutton-button" id="darkred" >
+          救助が必要
         </button>
-        <button className="safetybutton-button" id="green">
-        無事
+        <button className="safetybutton-button" id="green" >
+          無事
         </button>
       </div>
     </div>
   );
 };
 
-export default SafetyPage;
+
