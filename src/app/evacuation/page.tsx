66ca09904c70d6369a5e75a3ccf4b3acb16ad462
syumai6842 <syuumai6842@gmail.com>
2024-10-07 15:18:14 +0900

    add layout.tsx and some services

--- src/app/evacuation/page.tsx
+++ src/app/evacuation/page.tsx
@@ -1,9 +1,24 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import styles from './evacuationpage.module.css';
 import detectRotationAndUpload from './RotationDetector';
 
+const containerStyle = {
+  width: '100%',
+  height: '400px',  // マップの高さを適切に調整
+};
+
+const center = {
+  lat: 35.682839,  // 東京の緯度
+  lng: 139.759455, // 東京の経度
+};
+
 export default function Evacuation() {
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "", // APIキーを環境変数から取得
+  });
+
   useEffect(() => {
     // ページロード時にフッターを非表示にする
     const footerElement = document.querySelector('.Footer') as HTMLElement;
@@ -22,13 +37,25 @@ export default function Evacuation() {
     };
   }, []);
 
+  if (loadError) {
+    return <div>マップの読み込み中にエラーが発生しました。</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>マップを読み込んでいます...</div>;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>避難誘導</div>
       
       <div className={styles.mapPlaceholder}>
-        {/* マップ画像を挿入 */}
-        <img src="/images/map-picture.png" alt="Map" className={styles.mapImage} />
+        {/* Google Maps コンポーネント */}
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={12}
+        />
         
         {/* アイコンを画像の上にオーバーレイ表示 */}
         <div className={styles.icon} style={{top: "10%", right: "5%"}}>
